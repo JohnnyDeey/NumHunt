f531ad09a54f0a1e4f154d6091e37760662d47e2
JohnnyDeey <101556340+JohnnyDeey@users.noreply.github.com>
2026-08-07 12:57:35 +0200

    Improve manifest score - add shortcuts, lang, scope, display_override

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'numhunt-v2.1';
+const CACHE = 'numhunt-v2.2';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
@@ -23,6 +23,7 @@ self.addEventListener('fetch', e => {
     );
   }
 });
+
 
 
 
